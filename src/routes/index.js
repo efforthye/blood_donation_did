39989c14232f9ee4ea79371c7693b0724bfe0db3
efforthye <efforthye@gmail.com>
2024-05-23 03:38:50 +0900

    feat: send transaction ok

--- src/routes/index.js
+++ src/routes/index.js
@@ -17,7 +17,7 @@ const router = express.Router();
 // Web3 provider 설정
 const web3 = new Web3(process.env.API_URI);
 
-// 스마트 컨트랙트 ABI 및 주소 - build/contracts/BloodDonationRegistry.json - abi
+// 스마트 컨트랙트 ABI 및 주소
 const contractJSON = JSON.parse(fs.readFileSync(path.join(__dirname, '../../build/contracts/BloodDonationRegistry.json'), 'utf8'));
 const contractABI = contractJSON?.abi;
 const contractAddress = process.env.CONTRACT_ADDRESS;
@@ -25,52 +25,42 @@ const contract = new web3.eth.Contract(contractABI, contractAddress);
 
 // 이더리움 계정 주소
 const account = process.env.ACCOUNT;
-const privateKey = Buffer.from(process.env.PRIVATE_KEY.replace(/,/g, ''), 'hex'); // 프라이빗 키에서 쉼표 제거
+const privateKey = Buffer.from(process.env.PRIVATE_KEY.replace(/,/g, ''), 'hex');
 
 const getDonationDate = () => {
-  try {
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = String(today.getMonth() + 1).padStart(2, '0');
-    const day = String(today.getDate()).padStart(2, '0');
-    const donationDate = `${year}-${month}-${day}`;
-    return donationDate;
-  } catch (error) {
-    console.log({ func: 'getDonationDate', error });
-    throw error;
-  }
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, '0');
+  const day = String(today.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
 };
 
-// 숫자를 16진수로 변환하는 함수
 const toHex = (number) => {
   return '0x' + number.toString(16);
 };
+
+// BigInt를 처리하는 JSON.stringify replacer 함수
+const jsonReplacer = (key, value) =>
+  typeof value === 'bigint' ? value.toString() : value;
 
 // 헌혈자(보유자) DID 생성
 router.post('/create/donor', async (req, res, next) => {
   const { name, age, bloodType } = req.body;
 
   try {
-    // Generate key pair and sign data
     const { privateKey: donorPrivateKey, publicKey, publicKeyString, privateKeyString } = await generateECDSAKeyPair();
     const signedData = signToString(`Name: ${name}, Age: ${age}, Blood Type: ${bloodType}`, donorPrivateKey);
 
     const donationDate = getDonationDate();
-
-    // Create DID
     const method = 'polygon';
     const did = createDID(method, publicKeyString);
 
-    // Store data on the blockchain
-    const registerDonation = contract.methods.recordDonation(did, donationDate, bloodType); // recordDonation 함수 호출
+    const registerDonation = contract.methods.recordDonation(did, donationDate, bloodType);
     const gasPrice = BigInt(await web3.eth.getGasPrice());
-    console.log({ gasPrice });
     const gasEstimate = await registerDonation.estimateGas({ from: account });
-    console.log({ gasEstimate });
 
-    // EIP-1559 트랜잭션 데이터 준비
-    const maxPriorityFeePerGas = toHex(gasPrice * BigInt(10)); // 현재 가스 가격의 10배
-    const maxFeePerGas = toHex(gasPrice * BigInt(12)); // 현재 가스 가격의 6배
+    const maxPriorityFeePerGas = toHex(gasPrice * BigInt(20));
+    const maxFeePerGas = toHex(gasPrice * BigInt(22));
 
     const nonce = await web3.eth.getTransactionCount(account, 'pending');
     const txData = {
@@ -84,55 +74,54 @@ router.post('/create/donor', async (req, res, next) => {
       chainId: 137
     };
 
-    // txData 유효성 검사
     if (!/^0x[0-9a-fA-F]+$/.test(txData.maxPriorityFeePerGas) ||
         !/^0x[0-9a-fA-F]+$/.test(txData.maxFeePerGas) ||
         !/^0x[0-9a-fA-F]+$/.test(txData.gasLimit)) {
         throw new Error('Invalid transaction data');
     }
-    console.log({ txData });
 
-    // 트랜잭션 객체 생성
     const common = Common.custom({
       name: 'matic',
       networkId: 137,
       chainId: 137,
     }, {
       baseChain: 'mainnet',
-      hardfork: 'london' // EIP-1559를 사용하기 위해 'london' 하드포크를 사용
+      hardfork: 'london'
     });
-    console.log({ common });
 
     const tx = FeeMarketEIP1559Transaction.fromTxData(txData, { common });
-    console.log({ tx });
-
-    // 트랜잭션 서명
     const signedTx = tx.sign(privateKey);
     const serializedTx = signedTx.serialize();
-    const serializedTxHex = Buffer.from(serializedTx).toString('hex'); // 올바른 hex 형식으로 변환
-    console.log('Serialized Transaction Hex:', serializedTxHex);
+    const serializedTxHex = '0x'+Buffer.from(serializedTx).toString('hex');
+    console.log({signedTx, serializedTxHex});
 
-    // 서명된 트랜잭션 전송
-    web3.eth.sendSignedTransaction('0x' + serializedTxHex)
+    web3.eth.sendSignedTransaction(serializedTxHex)
       .on('receipt', (receipt) => {
-        console.log('Transaction receipt:', receipt);
-        res.send({ success: true, receipt, signedData, publicKey: publicKeyString, did });
+        res.send({ success: true, receipt: JSON.parse(JSON.stringify(receipt, jsonReplacer)), signedData, publicKey: publicKeyString, did });
       })
       .on('error', async (error) => {
-        console.error('Transaction error:', error);
+        if (error.message.includes('Transaction was not mined within 50 blocks')) {
+          const newNonce = await web3.eth.getTransactionCount(account, 'pending');
+          const newTxData = { ...txData, nonce: toHex(newNonce) };
+          const newTx = FeeMarketEIP1559Transaction.fromTxData(newTxData, { common });
+          const newSignedTx = newTx.sign(privateKey);
+          const newSerializedTx = newSignedTx.serialize();
+          const newSerializedTxHex = Buffer.from(newSerializedTx).toString('hex');
+          web3.eth.sendSignedTransaction('0x' + newSerializedTxHex)
+            .on('receipt', (newReceipt) => {
+              res.send({ success: true, receipt: JSON.parse(JSON.stringify(newReceipt, jsonReplacer)), signedData, publicKey: publicKeyString, did });
+            })
+            .on('error', (newError) => {
+              console.error('Transaction error on retry:', newError);
+              next(newError);
+            });
+        } else {
+          console.error('Transaction error:', error);
+          next(error);
+        }
       });
   } catch (error) {
-    next(error); // 에러를 다음 미들웨어로 전달
-  }
-});
-
-router.post('/create/certificate', (req, res, next) => {
-  const { donorId, certificateId, donationDate } = req.body;
-  try {
-    const { certificate, signature } = createDonationCertificate(donorId, certificateId, donationDate);
-    res.send({ certificate, signature });
-  } catch (error) {
-    next(error); // 에러를 다음 미들웨어로 전달
+    next(error);
   }
 });
 
@@ -144,22 +133,6 @@ router.post('/create/presentation', (req, res, next) => {
   } catch (error) {
     next(error); // 에러를 다음 미들웨어로 전달
   }
-});
-
-router.post('/create/did', (req, res, next) => {
-    try {
-        const { name, age, bloodType } = req.body;
-        
-        // Generate key pair
-        generateECDSAKeyPair().then(({ publicKeyString }) => {
-            // Create DID
-            const method = 'polygon';
-            const did = createDID(method, publicKeyString);
-            res.send({ name, age, bloodType, did });
-        }).catch(next); // 에러를 다음 미들웨어로 전달
-    } catch (error) {
-        next(error); // 에러를 다음 미들웨어로 전달
-    }
 });
 
 router.post('/use/certificate', (req, res, next) => {
